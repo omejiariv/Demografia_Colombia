27ce117891975f5fa58a7e864c7845234b959929
omejiariv <145706652+omejiariv@users.noreply.github.com>
2026-03-01 20:42:18 -0500

    Create calibracion.py

---
+++ engine/espacial/calibracion.py
@@ -0,0 +1,7 @@
+# engine/espacial/calibracion.py
+
+import numpy as np
+
+def calibrar_raster(raster_modelo, raster_worldpop):
+    factor = raster_worldpop.sum() / raster_modelo.sum()
+    return raster_modelo * factor
