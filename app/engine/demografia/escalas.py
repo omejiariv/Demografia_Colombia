a9a09f37a9eed3e30de9a1a80e7c47b8f2271886
omejiariv <145706652+omejiariv@users.noreply.github.com>
2026-03-01 22:36:50 -0500

    Create escalas.py

---
+++ app/engine/demografia/escalas.py
@@ -0,0 +1,8 @@
+# app/engine/demografia/escalas.py
+
+def redistribuir(*args, **kwargs):
+    """
+    Placeholder temporal.
+    Implementación real pendiente.
+    """
+    raise NotImplementedError("redistribuir() aún no implementado")
