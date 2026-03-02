cbd899d3cc2d081a6307d4a5fba3caf5031c4961
omejiariv <145706652+omejiariv@users.noreply.github.com>
2026-03-01 20:31:24 -0500

    Update modelos.py

--- engine/demografia/modelos.py
+++ engine/demografia/modelos.py
@@ -10,3 +10,19 @@ def modelo_logistico(t, K, r, t0):
     t0 : año de inflexión
     """
     return K / (1 + np.exp(-r * (t - t0)))
+
+def modelo_logistico_zona(t, params):
+    """
+    params = {
+        'K_urb':,
+        'r_urb':,
+        't0_urb':,
+        'K_rur':,
+        'r_rur':,
+        't0_rur':
+    }
+    """
+    urb = modelo_logistico(t, params["K_urb"], params["r_urb"], params["t0_urb"])
+    rur = modelo_logistico(t, params["K_rur"], params["r_rur"], params["t0_rur"])
+    return urb, rur, urb + rur
+
